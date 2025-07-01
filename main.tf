ab5ac16d7f289cf27e8c8fded5eefddf41822f0e
leydimadrid <leydi.madrid6@gmail.com>
2025-07-01 14:27:33 -0500

    Módulo Frontend y documentación lista

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "ec2_frontend_instance" {
   source = "./modules/ec2_frontend"
   ami_front_id = var.ami_front_id
   instance_type = var.instance_type
-  security_group_id = module.my_sg.security_group_id
+  security_group_id = module.sg_frontend.security_group_id
 }
 
 module "ec2_backend_instance" {
@@ -11,13 +11,13 @@ module "ec2_backend_instance" {
   instance_type = var.instance_type
   key_name = var.key_name
   git_repo_url = var.git_repo_url
-  security_group_id = module.my_sg.security_group_id
+  security_group_id = module.sg_backend.security_group_id
   region = var.region
 } 
   
 module "sg_frontend" {
   source  = "./modules/security_group"
-  puertos = [22, 5173]
+  puertos = [22, 5173, 3000]
 }
 
 module "sg_backend" {
